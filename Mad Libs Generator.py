4a939ba19af1d4b98dec20f376dd0b5283c57ac3
Yoshanth Raaj <48622984+mryoshanth@users.noreply.github.com>
2019-03-24 02:31:11 +0800

    Updated: Mad Libs Generator

--- Mad Libs Generator.py
+++ Mad Libs Generator.py
@@ -2,47 +2,23 @@ import random
 again = True
 
 def random_noun():
-    random_num = random.randint(0, 1)
-    if random_num == 0:
-        return noun[0]
-    else:
-        return noun[1]
+    order = random.choice(noun)
+    pos = noun.index(order)
+    string = str(noun.pop(pos))
+    return string
 
 def random_adjec():
-    random_num = random.randint(0, 1)
-    if random_num == 0:
-        return adjec[0]
-    else:
-        return adjec[1]
+    order = random.choice(adjec)
+    pos = adjec.index(order)
+    string = str(adjec.pop(pos))
+    return string
+
 
 def random_verb():
-    random_num = random.randint(0, 1)
-    if random_num == 0:
-        return verb[0]
-    else:
-        return verb[1]
-
-
-def process_string(madlib):
-    pre_process = newline(madlib.split())
-    for index, word in enumerate(pre_process):
-        if word == "NOUN":
-           pre_process[index] = random_noun()
-        elif word == "ADJEC":
-            pre_process[index] = random_adjec()
-        elif word == "VERB":
-            pre_process[index] = random_verb()
-    return " ".join(pre_process)
-
-def newline(list):
-    full_list = len(list)
-    start_list = 0
-    sub_list = 12
-    while start_list <= full_list:
-        list.insert(sub_list, "\n")
-        start_list += sub_list
-        sub_list += sub_list
-    return list
+    order = random.choice(verb)
+    pos = verb.index(order)
+    string = str(verb.pop(pos))
+    return string
 
 
 while again:
@@ -71,9 +47,11 @@ while again:
     print("\nHere's a Mad Libs story for you..")
     print("    ")
 
-    story = ("Post WW2, NOUN humbled themselves and started VERB harder. They focused their effort on developing NOUN \n"
-             "that had ADJEC features. Japan's rise through VERB is a ADJEC and memorable one.")
-    print(process_string(story))
+    story = ("Post WW2, "+random_noun()+" humbled themselves and started "+random_verb()+" harder. They focused their effort on developing \n"+random_noun()+
+                " that had "+random_adjec()+" features. Japan's rise through "+random_verb()+" is a "+random_adjec()+" and memorable one.")
+    print(story)
 
     print("\nWanna have another go?")
     again = ("y" or "yes") in input().lower()
+
+print("Good game, see ya!")
